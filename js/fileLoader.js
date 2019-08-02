f04d1c46aa63d077cc6bf3078684a3b17989ee56
zijia zhang <zijia.zhang99@gmail.com>
2019-08-01 22:14:38 -0700

    Fix File Loader.

--- js/fileLoader.js
+++ js/fileLoader.js
@@ -1,13 +1,15 @@
-var loadTextResource = function (url,callback){
+// Load a text resource from a file over the network
+var loadTextResource = function (url, callback) {
   var request = new XMLHttpRequest();
   request.open('GET', url , true);
-  request.onload = function() {
+  request.onload = function () {
     if (request.status < 200 || request.status > 299) {
       callback('Error: HTTP Status ' + request.status + ' on resource ' + url);
     } else {
       callback(null, request.responseText);
     }
   };
+  request.send();
 };
 
 var loadImage = function (url, callback) {
