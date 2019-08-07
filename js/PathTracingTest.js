3498bdbfe7aeceba75a04cca938f8963ee6ac2a1
zijia zhang <zijia.zhang99@gmail.com>
2019-08-06 22:12:19 -0700

    Change Request Anamation Frame; Isolate the Bounces.

--- js/PathTracingTest.js
+++ js/PathTracingTest.js
@@ -2,7 +2,7 @@ var renderVertexSource =
   ' attribute vec3 vertex;' +
   ' varying vec2 texCoord;' +
   ' void main() {' +
-  '   texCoord = vertex.xy*0.5+0.5;' +
+  '   texCoord = vertex.xy*0.5+0.5;' + //Map (-1,-1) (1,1) to (0,0),(1,1)
   '   gl_Position = vec4(vertex, 1.0);' +
   ' }';
 var renderFragmentSource =
@@ -39,7 +39,7 @@ var tracerVertexSource =
   '   initialRay = mix(mix(ray00, ray01, percent.y), mix(ray10, ray11, percent.y), percent.x);' +
   '   gl_Position = vec4(vertex, 1.0);' +
   ' }';
-
+Bounces = '100';
 var tracerFragmentSource =
   "precision highp float;" +
   "uniform vec3 eye;" +
@@ -63,7 +63,66 @@ var tracerFragmentSource =
   "} " +
   "vec3 normalForCube(vec3 hit, vec3 cubeMin, vec3 cubeMax) {   " +
   "if(hit.x < cubeMin.x + 0.0001) return vec3(-1.0, 0.0, 0.0);   " +
-  "else if(hit.x > cubeMax.x - 0.0001) return vec3(1.0, 0.0, 0.0);   else if(hit.y < cubeMin.y + 0.0001) return vec3(0.0, -1.0, 0.0);   else if(hit.y > cubeMax.y - 0.0001) return vec3(0.0, 1.0, 0.0);   else if(hit.z < cubeMin.z + 0.0001) return vec3(0.0, 0.0, -1.0);   else return vec3(0.0, 0.0, 1.0); } float intersectSphere(vec3 origin, vec3 ray, vec3 sphereCenter, float sphereRadius) {   vec3 toSphere = origin - sphereCenter;   float a = dot(ray, ray);   float b = 2.0 * dot(toSphere, ray);   float c = dot(toSphere, toSphere) - sphereRadius*sphereRadius;   float discriminant = b*b - 4.0*a*c;   if(discriminant > 0.0) {     float t = (-b - sqrt(discriminant)) / (2.0 * a);     if(t > 0.0) return t;   }   return 10000.0; } vec3 normalForSphere(vec3 hit, vec3 sphereCenter, float sphereRadius) {   return (hit - sphereCenter) / sphereRadius; } float random(vec3 scale, float seed) {   return fract(sin(dot(gl_FragCoord.xyz + seed, scale)) * 43758.5453 + seed); } vec3 cosineWeightedDirection(float seed, vec3 normal) {   float u = random(vec3(12.9898, 78.233, 151.7182), seed);   float v = random(vec3(63.7264, 10.873, 623.6736), seed);   float r = sqrt(u);   float angle = 6.283185307179586 * v;   vec3 sdir, tdir;   if (abs(normal.x)<.5) {     sdir = cross(normal, vec3(1,0,0));   } else {     sdir = cross(normal, vec3(0,1,0));   }   tdir = cross(normal, sdir);   return r*cos(angle)*sdir + r*sin(angle)*tdir + sqrt(1.-u)*normal; } vec3 uniformlyRandomDirection(float seed) {   float u = random(vec3(12.9898, 78.233, 151.7182), seed);   float v = random(vec3(63.7264, 10.873, 623.6736), seed);   float z = 1.0 - 2.0 * u;   float r = sqrt(1.0 - z * z);   float angle = 6.283185307179586 * v;   return vec3(r * cos(angle), r * sin(angle), z); } vec3 uniformlyRandomVector(float seed) {   return uniformlyRandomDirection(seed) * sqrt(random(vec3(36.7539, 50.3658, 306.2759), seed)); } float shadow(vec3 origin, vec3 ray) { vec2 tCube4 = intersectCube(origin, ray, cubeMin4, cubeMax4); if(tCube4.x > 0.0 && tCube4.x < 1.0 && tCube4.x < tCube4.y) return 0.0;   return 1.0; } vec3 calculateColor(vec3 origin, vec3 ray, vec3 light) {   vec3 colorMask = vec3(1.0);   vec3 accumulatedColor = vec3(0.0);   for(int bounce = 0; bounce < 5; bounce++) {     vec2 tRoom = intersectCube(origin, ray, roomCubeMin, roomCubeMax); vec2 tCube4 = intersectCube(origin, ray, cubeMin4, cubeMax4);     float t = 10000.0;     if(tRoom.x < tRoom.y) t = tRoom.y; if(tCube4.x > 0.0 && tCube4.x < tCube4.y && tCube4.x < t) t = tCube4.x;     vec3 hit = origin + ray * t;     vec3 surfaceColor = vec3(0.75);     float specularHighlight = 0.0;     vec3 normal;     if(t == tRoom.y) {       normal = -normalForCube(hit, roomCubeMin, roomCubeMax); if(hit.x < -0.9999) surfaceColor = vec3(0.1, 0.5, 1.0); else if(hit.x > 0.9999) surfaceColor = vec3(0.0, 0.9, 0.1); ray = cosineWeightedDirection(timeSinceStart + float(bounce), normal);     } else if(t == 10000.0) {       break;     } else {       if(false) ; else if(t == tCube4.x && tCube4.x < tCube4.y) normal = normalForCube(hit, cubeMin4, cubeMax4); ray = reflect(ray, normal); vec3 reflectedLight = normalize(reflect(light - hit, normal)); specularHighlight = max(0.0, dot(reflectedLight, normalize(hit - origin))); specularHighlight = 2.0 * pow(specularHighlight, 20.0);     }     vec3 toLight = light - hit;     float diffuse = max(0.0, dot(normalize(toLight), normal));     float shadowIntensity = shadow(hit + normal * 0.0001, toLight);     colorMask *= surfaceColor;     accumulatedColor += colorMask * (0.5 * diffuse * shadowIntensity);     accumulatedColor += colorMask * specularHighlight * shadowIntensity;     origin = hit;   }   return accumulatedColor; } void main() {   vec3 newLight = light + uniformlyRandomVector(timeSinceStart - 53.0) * 0.1;   vec3 texture = texture2D(texture, gl_FragCoord.xy / 512.0).rgb;   gl_FragColor = vec4(mix(calculateColor(eye, initialRay, newLight), texture, textureWeight), 1.0); }"
+  "else if(hit.x > cubeMax.x - 0.0001) return vec3(1.0, 0.0, 0.0);" +
+  "   else if(hit.y < cubeMin.y + 0.0001) return vec3(0.0, -1.0, 0.0);" +
+  "   else if(hit.y > cubeMax.y - 0.0001) return vec3(0.0, 1.0, 0.0);" +
+  "   else if(hit.z < cubeMin.z + 0.0001) return vec3(0.0, 0.0, -1.0);" +
+  "   else return vec3(0.0, 0.0, 1.0);" +
+  " } float intersectSphere(vec3 origin, vec3 ray, vec3 sphereCenter, float sphereRadius) {" +
+  "   vec3 toSphere = origin - sphereCenter;" +
+  "   float a = dot(ray, ray);" +
+  "   float b = 2.0 * dot(toSphere, ray);" +
+  "   float c = dot(toSphere, toSphere) - sphereRadius*sphereRadius;" +
+  "   float discriminant = b*b - 4.0*a*c; " +
+  "  if(discriminant > 0.0) {" +
+  "     float t = (-b - sqrt(discriminant)) / (2.0 * a);" +
+  "     if(t > 0.0) return t;  " +
+  " }   return 10000.0;" +
+  " } vec3 normalForSphere(vec3 hit, vec3 sphereCenter, float sphereRadius) {  " +
+  " return (hit - sphereCenter) / sphereRadius;" +
+  " } float random(vec3 scale, float seed) {" +
+  "   return fract(sin(dot(gl_FragCoord.xyz + seed, scale)) * 43758.5453 + seed); " +
+  "} " +
+  "vec3 cosineWeightedDirection(float seed, vec3 normal) { " +
+  "  float u = random(vec3(12.9898, 78.233, 151.7182), seed); " +
+  "  float v = random(vec3(63.7264, 10.873, 623.6736), seed); " +
+  "  float r = sqrt(u);" +
+  "   float angle = 6.283185307179586 * v;" +
+  "   vec3 sdir, tdir;   if (abs(normal.x)<.5) { " +
+  "    sdir = cross(normal, vec3(1,0,0));   }" +
+  " else { " +
+  "    sdir = cross(normal, vec3(0,1,0));" +
+  "   }   " +
+  "tdir = cross(normal, sdir);" +
+  "   return r*cos(angle)*sdir + r*sin(angle)*tdir + sqrt(1.-u)*normal; " +
+  "} " +
+  "vec3 uniformlyRandomDirection(float seed) { " +
+  "  float u = random(vec3(12.9898, 78.233, 151.7182), seed);" +
+  "   float v = random(vec3(63.7264, 10.873, 623.6736), seed);" +
+  "   float z = 1.0 - 2.0 * u;" +
+  "   float r = sqrt(1.0 - z * z);" +
+  "   float angle = 6.283185307179586 * v;" +
+  "   return vec3(r * cos(angle), r * sin(angle), z); } " +
+  "vec3 uniformlyRandomVector(float seed) {   return uniformlyRandomDirection(seed) * sqrt(random(vec3(36.7539, 50.3658, 306.2759), seed)); } " +
+  "float shadow(vec3 origin, vec3 ray) { vec2 tCube4 = intersectCube(origin, ray, cubeMin4, cubeMax4); if(tCube4.x > 0.0 && tCube4.x < 1.0 && tCube4.x < tCube4.y) return 0.0;   return 1.0; } " +
+  "vec3 calculateColor(vec3 origin, vec3 ray, vec3 light) { " +
+  "  vec3 colorMask = vec3(1.0);   " +
+  "  vec3 accumulatedColor = vec3(0.0);   " +
+  "for(int bounce = 0; bounce < "+Bounces +"; bounce++) { " +
+  "  vec2 tRoom = intersectCube(origin, ray, roomCubeMin, roomCubeMax); " +
+  "vec2 tCube4 = intersectCube(origin, ray, cubeMin4, cubeMax4);" +
+  " float t = 10000.0;" +
+  "  if(tRoom.x < tRoom.y) t = tRoom.y;" +
+  "if(tCube4.x > 0.0 && tCube4.x < tCube4.y && tCube4.x < t) t = tCube4.x;" +
+  "     vec3 hit = origin + ray * t;     vec3 surfaceColor = vec3(0.75); " +
+  "    float specularHighlight = 0.0;" +
+  "     vec3 normal;" +
+  "     if(t == tRoom.y) {  " +
+  "     normal = -normalForCube(hit, roomCubeMin, roomCubeMax);" +
+  " if(hit.x < -0.9999) surfaceColor = vec3(0.1, 0.5, 1.0);" +
+  " else if(hit.x > 0.9999) surfaceColor = vec3(0.0, 0.9, 0.1); " +
+  "ray = cosineWeightedDirection(timeSinceStart + float(bounce), normal);" +
+  "     } else if(t == 10000.0) {       break;     } else {       if(false) ; else if(t == tCube4.x && tCube4.x < tCube4.y) normal = normalForCube(hit, cubeMin4, cubeMax4); ray = reflect(ray, normal); vec3 reflectedLight = normalize(reflect(light - hit, normal)); specularHighlight = max(0.0, dot(reflectedLight, normalize(hit - origin))); specularHighlight = 2.0 * pow(specularHighlight, 20.0);     }     vec3 toLight = light - hit;     float diffuse = max(0.0, dot(normalize(toLight), normal));     float shadowIntensity = shadow(hit + normal * 0.0001, toLight);     colorMask *= surfaceColor;     accumulatedColor += colorMask * (0.5 * diffuse * shadowIntensity);     accumulatedColor += colorMask * specularHighlight * shadowIntensity;     origin = hit;   }   return accumulatedColor; } void main() {   vec3 newLight = light + uniformlyRandomVector(timeSinceStart - 53.0) * 0.1;   vec3 texture = texture2D(texture, gl_FragCoord.xy / 512.0).rgb;   gl_FragColor = vec4(mix(calculateColor(eye, initialRay, newLight), texture, textureWeight), 1.0); }"
 
 var gl;
 var ui;
@@ -270,12 +329,18 @@ var Initialize = function () {
   var start = new Date();
 
   var tick = function(time){
+    temp = document.getElementById("angle").value;
+    if(temp!==angleY){
+      sampleCount = 0;
+      angleY = temp;
+    }
+
     eye.elements[0] = zoomZ * Math.sin(angleY) * Math.cos(angleX);
     eye.elements[1] = zoomZ * Math.sin(angleX);
     eye.elements[2] = zoomZ * Math.cos(angleY) * Math.cos(angleX);
 
     var modelview = makeLookAt(eye.elements[0], eye.elements[1], eye.elements[2], 0, 0, 0, 0, 1, 0);
-    var projection = makePerspective(55, canvas.width/canvas.height, 0.1, 100);
+    var projection = makePerspective(55, 1, 0.1, 100);
     var modelviewProjection = projection.multiply(modelview);
     var jitter = Matrix.Translation(Vector.create([Math.random() * 2 - 1, Math.random() * 2 - 1, 0]).multiply(1 / 512));
     var t = jitter.multiply(modelviewProjection);
@@ -314,13 +379,14 @@ var Initialize = function () {
     gl.vertexAttribPointer(pathTracerRenderVertexAttrib,2,gl.FLOAT,false,0,0);
     gl.drawArrays(gl.TRIANGLE_STRIP,0,4);
 
+    requestAnimationFrame(function () {
+      tick((new Date() - start)/1000);});
 
   };
 
 
-  setInterval(function () {
-    tick((new Date() - start)/1000);},1000/60
-  );
+  requestAnimationFrame(function () {
+    tick((new Date() - start)/1000);});
 
 
 
